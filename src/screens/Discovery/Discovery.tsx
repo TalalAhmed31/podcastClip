afea2bfb6078e7c7f9987ebd421ab5eb8cb2c149
Talal Ahmed <talalsidd31@gmail.com>
2025-04-03 18:14:24 +0500

    navigator screens

---
+++ src/screens/Discovery/Discovery.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Discovery() {
+  return (
+    <View>
+      <Text>Discovery</Text>
+    </View>
+  )
+}
